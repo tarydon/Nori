b08e4d3756c465e8e561e3c892a3e6339dcf9d40
arvind <arvind@metamation.com>
2025-03-27 22:55:51 +0530

    Added several pages in WPFDemo

---
+++ Demos/WPFDemo/MeshScene.cs
@@ -0,0 +1,18 @@
+﻿namespace WPFDemo;
+using Nori;
+
+class MeshScene : Scene3 {
+   public MeshScene () {
+      mMesh = CMesh.LoadTMesh ($"{Lib.DevRoot}/TData/Geom/CMesh/part.tmesh");
+      Bound = mMesh.Bound;
+      Root = new MeshVN (mMesh);
+   }
+   CMesh mMesh;
+
+   public override Color4 BgrdColor => Color4.Gray (96);
+}
+
+class MeshVN (CMesh mesh) : VNode {
+   public override void SetAttributes () => Lux.Color = new Color4 (255, 255, 128);
+   public override void Draw () => Lux.Mesh (mesh, EShadeMode.Phong);
+}
