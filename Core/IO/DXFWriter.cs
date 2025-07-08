e18f5f95934dd04cb62d1c63e1eb54aebac76cd4
naveents <naveen.thangavel@trumpf.com>
2025-07-08 18:35:06 +0530

    Review fix

--- Core/IO/DXFWriter.cs
+++ Core/IO/DXFWriter.cs
@@ -45,9 +45,11 @@ public class DXFWriter (Dwg2 dwg) {
       var layers = mDwg.Layers.ToList ();
       if (mDwg.Ents.Any (a => a is E2Bendline)) {
          mBend = layers.FirstOrDefault (a => a.Name.EqIC ("BEND"));
-         if (mMBend == null) layers.Add (mBend = new Layer2 ("BEND", Color4.Green, ELineType.Dot));
          mMBend = layers.FirstOrDefault (a => a.Name.EqIC ("MBEND"));
-         if (mMBend == null) layers.Add (mMBend = new Layer2 ("MBEND", Color4.Green, ELineType.DashDotDot));
+         mDwg.Ents.OfType<E2Bendline> ().ForEach (a => {
+            if (mBend is null && a.Angle > 0) layers.Add (mBend = new Layer2 ("BEND", Color4.Green, ELineType.Dot));
+            else if (mMBend is null && a.Angle < 0) layers.Add (mMBend = new Layer2 ("MBEND", Color4.Green, ELineType.DashDotDot));
+         });
       }
       Out ($" 0\nTABLE\n 2\nLAYER\n 70\n{layers.Count}\n");
       foreach (var layer in layers) {
@@ -195,6 +197,7 @@ public class DXFWriter (Dwg2 dwg) {
 
    int OutBendLine (E2Bendline eb) {
       Point2 pa = eb.Pts[0], pb = eb.Pts[^1];
+      if (Lib.Testing) (pa, pb) = (pa.R6 (), pb.R6 ());
       var layer = eb.Angle < 0 ? mMBend : mBend;
       Out ($"0\nLINE\n8\n{layer?.Name}\n10\n{pa.X}\n20\n{pa.Y}\n11\n{pb.X}\n21\n{pb.Y}\n");
       Out ($"1000\nBEND_ANGLE:{eb.Angle.R2D ()}\n");
