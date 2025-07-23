a7ec0da833ac6ebbebc0be0896f84d3b81150923
Dinesh N <dinesh.n@metamation.com>
2025-07-23 17:37:11 +0530

    Conflicts resolved

--- Test/IO/TDXFIO.cs
+++ Test/IO/TDXFIO.cs
@@ -180,8 +180,8 @@ class DXFTests {
    public void Test21 () {
       var dwg = new Dwg2 ();
       dwg.Add (Poly.Rectangle (0, 0, 60, 50));
-      dwg.Add (new E2Bendline (dwg, Point2.List (40, 0, 40, 50), Lib.HalfPI, 2, 0.42));
-      dwg.Add (new E2Bendline (dwg, Point2.List (20, 0, 20, 50), -Lib.HalfPI, 2, 0.42));
+      dwg.Add (new E2Bendline (dwg, Point2.List (40, 0, 40, 50), Lib.HalfPI, 2, 0.42, 1));
+      dwg.Add (new E2Bendline (dwg, Point2.List (20, 0, 20, 50), -Lib.HalfPI, 2, 0.42, 1));
       DXFWriter.SaveFile (dwg, NT.TmpDXF);
       Assert.TextFilesEqual1 ("IO/DXF/Out/BendLine.dxf", NT.TmpDXF);
    }
