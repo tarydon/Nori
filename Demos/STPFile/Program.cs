1fc644de9e686af1b728c5958d0d473df605c957
arvind <arvind@fastmail.in>
2025-08-24 08:46:28 +0530

    Started work on STEP reader

---
+++ Demos/STPFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using Nori;
+
+internal class Program {
+   static void Main (string[] args) {
+      Lib.Init ();
+      var files = Directory.GetFiles ("C:\\Step", "*.step", SearchOption.TopDirectoryOnly);
+      foreach (var file in files) {
+         var step = new STEPReader (file);
+         try {
+            step.Parse ();
+            File.Move (file, "C:\\Step\\Good\\" + Path.GetFileName (file));
+         } catch (Exception e) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine (e);
+            Console.ResetColor ();
+            File.Move (file, "C:\\Step\\Bad\\" + Path.GetFileName (file));
+         }
+      }
+   }
+}
