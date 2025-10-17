8bb127b8c66bd640603d279ff94a1faae1304e75
arvind <arvind@fastmail.in>
2025-10-17 08:58:03 +0530

    Refined DwgStitch with overlapping cells

--- Core/Drawing/DwgStitch.cs
+++ Core/Drawing/DwgStitch.cs
@@ -7,8 +7,10 @@ namespace Nori;
 class DwgStitcher {
    public DwgStitcher (Dwg2 dwg, double threshold = 1e-3) {
       mDwg = dwg;
-      mComp = new (mThreshold = threshold);
-      mEnds = new Dictionary<Point2, E2Poly> (mComp);
+      mComp1 = new (mThreshold = threshold, 0);
+      mComp2 = new (threshold, threshold / 2);
+      mEnds1 = new Dictionary<Point2, E2Poly> (mComp1);
+      mEnds2 = new Dictionary<Point2, E2Poly> (mComp2);
    }
 
    public void Process () {
@@ -20,7 +22,7 @@ class DwgStitcher {
             if (e2p.Poly.TryCleanup (out var tmp)) e2p = e2p.With (tmp);
             if (e2p.Poly.IsOpen) ents.Add (e2p);
             else mDone.Add (ent);
-         } else 
+         } else
             mDone.Add (ent);
       }
 
@@ -41,7 +43,7 @@ class DwgStitcher {
             // Pick each endpoint of 'final' and see if there is an (already seen) fragment
             // that can attach to it
             var pt = end == 0 ? poly.A : poly.B;
-            if (mEnds.TryGetValue (pt, out E2Poly? other)) {
+            if (mEnds1.TryGetValue (pt, out E2Poly? other) || mEnds2.TryGetValue (pt, out other)) {
                if (final.TryAppend (other.Poly, out var tmp, mThreshold)) {
                   // If so, remove this endpoint from the list of free-floating ends, and
                   // if the newly joined result is now self-closing, we are done.
@@ -65,20 +67,22 @@ class DwgStitcher {
 
    // Implementation -----------------------------------------------------------
    void AddEnds (E2Poly ent) {
-      mEnds[ent.Poly.A] = ent; mEnds[ent.Poly.B] = ent;
+      mEnds1[ent.Poly.A] = ent; mEnds1[ent.Poly.B] = ent;
+      mEnds2[ent.Poly.B] = ent; mEnds2[ent.Poly.B] = ent;
    }
 
    void AddRemaining () {
-      mDone.AddRange (mEnds.Values.Distinct ());
-      mEnds.Clear ();
+      mDone.AddRange (mEnds1.Values.Distinct ());
+      mEnds1.Clear (); mEnds2.Clear ();
    }
 
    void RemoveEnds (E2Poly ent) {
-      mEnds.Remove (ent.Poly.A); mEnds.Remove (ent.Poly.B);
+      mEnds1.Remove (ent.Poly.A); mEnds1.Remove (ent.Poly.B);
+      mEnds2.Remove (ent.Poly.A); mEnds2.Remove (ent.Poly.B);
    }
 
    bool TryClose (E2Poly template , Poly poly) {
-      if (mComp.Equals (poly.A, poly.B)) {
+      if (mComp1.Equals (poly.A, poly.B) || mComp2.Equals (poly.A, poly.B)) {
          mDone.Add (template.With (poly.Close ().Clean ()));
          return true;
       }
@@ -88,16 +92,16 @@ class DwgStitcher {
    // Private data -------------------------------------------------------------
    readonly Dwg2 mDwg;
    readonly double mThreshold;
-   readonly Dictionary<Point2, E2Poly> mEnds;
-   readonly PointComparer mComp;
+   readonly Dictionary<Point2, E2Poly> mEnds1, mEnds2;
+   readonly PointComparer mComp1, mComp2;
    readonly List<Ent2> mDone = [];
 }
 
-class PointComparer (double threshold) : IEqualityComparer<Point2> {
+class PointComparer (double threshold, double offset) : IEqualityComparer<Point2> {
    public bool Equals (Point2 a, Point2 b)
-      => a.X.Round (threshold) == b.X.Round (threshold)
-      && a.Y.Round (threshold) == b.Y.Round (threshold);
+      => (a.X + offset).Round (threshold) == (b.X + offset).Round (threshold)
+      && (a.Y + offset).Round (threshold) == (b.Y + offset).Round (threshold);
 
    public int GetHashCode (Point2 a)
-      => HashCode.Combine (a.X.Round (threshold), a.Y.Round (threshold));
+      => HashCode.Combine ((a.X + offset).Round (threshold), (a.Y + offset).Round (threshold));
 }
