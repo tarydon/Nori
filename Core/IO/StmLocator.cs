1dda3e67b094eddc4cf64f34c31dc54eb5aca663
vikram <117505871+vikrampg1710-tmm@users.noreply.github.com>
2025-10-22 19:16:21 +0530

    Implemented ZipStmLocator class

--- Core/IO/StmLocator.cs
+++ Core/IO/StmLocator.cs
@@ -3,6 +3,7 @@
 // ║║║║╬║╔╣║ Implementations of IStmLocator interface: FileStmLocator
 // ╚╩═╩═╩╝╚╝ ───────────────────────────────────────────────────────────────────────────────────────
 namespace Nori;
+using System.IO.Compression;
 
 #region class FileStmLocator -----------------------------------------------------------------------
 public class FileStmLocator (string prefix, string baseDir) : IStmLocator {
@@ -14,6 +15,20 @@ public class FileStmLocator (string prefix, string baseDir) : IStmLocator {
       if (!name.StartsWith (prefix)) return null;
       string fullName = Path.Combine (baseDir, name[prefix.Length..]);
       return Path.Exists (fullName) ? File.OpenRead (fullName) : null;
+   }
+}
+#endregion
+
+#region class ZipStmLocator ------------------------------------------------------------------------
+public class ZipStmLocator (string prefix, string zipFile) : IStmLocator {
+   // Properties ---------------------------------------------------------------
+   public string Prefix => prefix;
+
+   // Methods ------------------------------------------------------------------
+   public Stream? Open (string name) {
+      if (!name.StartsWith (prefix) || !Path.Exists (zipFile)) return null;
+      var archive = new ZipArchive (File.OpenRead (zipFile));
+      return archive.GetEntry (name[prefix.Length..])?.Open ();
    }
 }
 #endregion
