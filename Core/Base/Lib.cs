a1a14e48754796a1ef0f8307a34e5fc532193f08
ShakthyTMM <shakthy.r@metamation.com>
2025-10-28 11:47:09 +0530

    Minor

--- Core/Base/Lib.cs
+++ Core/Base/Lib.cs
@@ -104,8 +104,7 @@ public static class Lib {
       if (!sInited) {
          sInited = true;
          var file = GetLocalFile ("Nori.wad");
-         if (File.Exists (file)) Register (new ZipStmLocator ("nori:", file));
-         else Register (new FileStmLocator ("nori:", $"{DevRoot}/Wad/"));
+         Register (File.Exists (file) ? new ZipStmLocator ("nori:", file) : new FileStmLocator ("nori:", $"{DevRoot}/Wad/"));
          AddAssembly (Assembly.GetExecutingAssembly ());
          AddNamespace ("Nori"); AddNamespace ("System"); AddNamespace ("System.Collections.Generic");
       }
