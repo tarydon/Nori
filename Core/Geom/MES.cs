42c6067ff4e118a5bc2bec0370fd3979ed1903f8
rajesh <rajesh@metamation.com>
2025-12-12 21:58:51 +0530

    Added MinSphere demo and tests

--- Core/Geom/MES.cs
+++ Core/Geom/MES.cs
@@ -1,24 +1,30 @@
 ﻿// ────── ╔╗
-// ╔═╦╦═╦╦╬╣ MEs.cs
+// ╔═╦╦═╦╦╬╣ MES.cs
 // ║║║║╬║╔╣║ Implements minimum enclosing circle and sphere
 // ╚╩═╩═╩╝╚╝ ───────────────────────────────────────────────────────────────────────────────────────
 using static Nori.Lib;
 namespace Nori;
 
 #region struct MinCircle ---------------------------------------------------------------------------
-// A minimal circle with a radius and center.
-public readonly struct MinCircle (double radius, Point2 center) {
-   // Center of the circle.
-   readonly public Point2 Center = center;
-   // Radius of the circle.
-   readonly public double Radius = radius;
-   // Is this a valid circle?
+/// <summary>Implements a minimum circle for a given set of points.</summary>
+public readonly struct MinCircle {
+   // Private constructor to construct a circle from radius and center.
+   MinCircle (double radius, Point2 center) { 
+      Radius = radius; 
+      Center = center; 
+      RSqr = radius > 0 ? (radius * radius) + Epsilon : 0;
+   }
+
+   /// <summary>Center of the circle.</summary>
+   readonly public Point2 Center;
+   /// <summary>Radius of the circle.</summary>
+   readonly public double Radius;
+   /// <summary>Is this a valid circle?</summary>
    public bool OK => Radius > Epsilon;
-   // Checks if a circle contains a given point
+   /// <summary>Checks if a circle contains a given point </summary>
    public bool Contains (in Point2 pt) => OK && Center.DistToSq (pt) < RSqr;
-   
-   // Checks if a circle contains a set of points. 
-   // Like Contains (Point2), it avoid Sqrt for better performance.
+   /// <summary>Checks if a circle contains a set of points.</summary>
+   /// Like Contains (Point2), it avoid Sqrt for better performance.
    public bool Contains (ReadOnlySpan<Point2> pts) {
       if (!OK) return false;
       for (int i = 0; i < pts.Length; i++)
@@ -30,16 +36,23 @@ public readonly struct MinCircle (double radius, Point2 center) {
    // Is this circle equal the other circle?
    public bool EQ (in MinCircle other) => Radius.EQ (other.Radius) && Center.EQ (other.Center);
 
-   public override string ToString () => $"({Radius.R6 ()}, {Center})";
+   public override string ToString () => $"{Radius.R6 ()}, {Center}";
 
-   // Constructs a minimum-enclosing-cicle from a given set of points.
+   /// <summary>Constructs a mimimum-enclosing-cicle from a given set of points.</summary>
+   /// Recursively computes the minimum enclosing circle in O(n) expected time.
+   /// https://en.wikipedia.org/wiki/Smallest-circle_problem
+   /// <code>
+   /// Point2[] points = [(100, 200), (400, 300), (200, 500), (100, 0)];
+   /// MinCircle circle = MinCircle.From (points);
+   /// </code>
+   /// <param name="pts">The input points</param>
    public static MinCircle From (ReadOnlySpan<Point2> pts) {
-      return MEC (pts, []);
+      return pts.Length <= 3 ? MEC ([], pts) : MEC (pts, []);
 
       static MinCircle MEC (ReadOnlySpan<Point2> pts, ReadOnlySpan<Point2> outer) {
          MinCircle c = outer.Length switch {
             2 => From (outer[0], outer[1]), // Circle from diameter
-            3 => From (outer[0], outer[1], outer[2]), // Circum-circle
+            3 => Find (outer), // Possibly Circumcircle
             _ => Nil
          };
          if (pts.Length == 0 || outer.Length == 3) return c;
@@ -51,20 +64,36 @@ public readonly struct MinCircle (double radius, Point2 center) {
                (0, _) => MEC (pts[..i], [pt]),
                (1, false) => From (outer[0], pt), // Circle from diameter
                (1, _) => MEC (pts[..i], [outer[0], pt]),
-               (2, false) => From (outer[0], outer[1], pt), // Circum-circle
                (2, _) => MEC (pts[..i], [outer[0], outer[1], pt]),
                _ => throw new Exception ("Not expecting this")
             };
          }
          return c;
       }
+
+      // Searches for the Minimum Enclosing Circle (MEC) for the given set of points.
+      static MinCircle Find (ReadOnlySpan<Point2> pts) {
+         if (pts.Length != 3) throw new ArgumentException ("Expecting three points to compute circumcircle.");
+         // Circum-circle from three points
+         MinCircle best = From (pts[0], pts[1], pts[2]);
+         // Find MEC from pair of points
+         for (int i = 0; i < pts.Length - 1; i++)
+            for (int j = i + 1; j < pts.Length; j++)
+               Consider (From (pts[i], pts[j]), pts);
+         return best;
+
+         void Consider (in MinCircle c, ReadOnlySpan<Point2> pts) {
+            if ((!best.OK || c.Radius < (best.Radius - Epsilon)) && c.Contains (pts))
+               best = c;
+         }
+      }
    }
 
-   // Constructs a circle with endpoints specifying the diameter.
+   /// <summary>Constructs a circle with two endpoints specifying the diameter.</summary>
    public static MinCircle From (Point2 a, Point2 b) =>
       new (a.DistTo (b) / 2, new ((a.X + b.X) / 2, (a.Y + b.Y) / 2));
 
-   // Constructs the circumcircle from three non-collinear points
+   /// <summary>Constructs the circumcircle from three non-collinear points</summary>
    internal static MinCircle From (Point2 a, Point2 b, Point2 c) {
       // To make computations easier, translate points by vector '-a' so 'a' becomes (0, 0).
       var ba = b - a; var ca = c - a;
@@ -75,30 +104,38 @@ public readonly struct MinCircle (double radius, Point2 center) {
          var B = bx * bx + by * by;
          var C = cx * cx + cy * cy;
          // Denominator
-         var D = bx * cy - by * cx;
-         return new Point2 (a.X + (cy * B - by * C) / (2 * D), a.Y + (bx * C - cx * B) / (2 * D));
+         var D = 2 * (bx * cy - by * cx);
+         return new Point2 (a.X + (cy * B - by * C) / D, a.Y + (bx * C - cx * B) / D);
       }
    }
-
+   /// <summary>The uninitialized 'Nil' circle.</summary>
    public readonly static MinCircle Nil = new (-1, Point2.Nil);
-   readonly double RSqr = radius * radius + Epsilon;
+   readonly double RSqr;
 }
 #endregion
 
 #region struct MinSphere ---------------------------------------------------------------------------
-// A minimal sphere with a radius and center.
-public readonly struct MinSphere (double radius, Point3 center) {
-   // Center of the sphere.
-   readonly public Point3 Center = center;
-   // Radius of the sphere.
-   readonly public double Radius = radius;
-   // Is this a valid sphere?
+/// <summary>Implements a minimum sphere for a given set of points.</summary>
+/// This is an implementation of Welzl's algorithm for finding the minimum enclosing sphere.
+public readonly struct MinSphere {
+   // Private constructor to construct a sphere from radius and center.
+   MinSphere (double radius, Point3 center) {
+      Radius = radius;
+      Center = center;
+      RSqr = radius > 0 ? (radius * radius) + Epsilon : 0;
+   }
+
+   /// <summary>Center of the sphere.</summary>
+   readonly public Point3 Center;
+   /// <summary>Radius of the sphere.</summary>
+   readonly public double Radius;
+   /// <summary>Is this a valid sphere?</summary>
    public bool OK => Radius > Epsilon;
-   // Checks if a sphere contains a given point
+   /// <summary>Checks if a sphere contains a given point.</summary>
    public bool Contains (in Point3 pt) => OK && Center.DistToSq (pt) < RSqr;
 
-   // Checks if a sphere contains a set of points. 
-   // Like Contains (Point3), it avoids Sqrt for better performance.
+   /// <summary>Checks if a sphere contains a set of points.</summary>
+   /// Like Contains (Point3), it avoids Sqrt for better performance.
    public bool Contains (ReadOnlySpan<Point3> pts) {
       if (!OK) return false;
       for (int i = 0; i < pts.Length; i++)
@@ -107,20 +144,26 @@ public readonly struct MinSphere (double radius, Point3 center) {
       return true;
    }
 
-   // Is this sphere equal the other sphere?
+   /// <summary>Is this sphere equal the other sphere?</summary>
    public bool EQ (in MinSphere other) => Radius.EQ (other.Radius) && Center.EQ (other.Center);
 
-   public override string ToString () => $"({Radius.R6 ()}, {Center})";
+   public override string ToString () => $"{Radius.R6 ()}, {Center}";
 
-   // Constructs a mimimum-enclosing-cicle from a given set of points.
+   /// <summary>Constructs a mimimum-enclosing-cicle from a given set of points.</summary>
+   /// Recursively computes the minimum enclosing sphere in O(n) expected time.
+   /// <code>
+   /// Point3[] points = [(100, 200, 300), (400, 500, 600), (500, 200, 100), (100, 0, 500)];
+   /// MinSphere sphere = MinSphere.From (points);
+   /// </code>
+   /// <param name="pts">The input points.</param>
    public static MinSphere From (ReadOnlySpan<Point3> pts) {
-      return MES (pts, []);
+      return pts.Length <= 4 ? MES ([], pts) : MES (pts, []);
 
       static MinSphere MES (ReadOnlySpan<Point3> pts, ReadOnlySpan<Point3> outer) {
          MinSphere s = outer.Length switch {
             2 => From (outer[0], outer[1]), // Sphere from two boundary points
             3 => From (outer[0], outer[1], outer[2]), // Sphere from 3 boundary points
-            4 => From (outer[0], outer[1], outer[2], outer[3]), // Circumsphere from boundary points
+            4 => Find (outer), // Possibly circumsphere from boundary points
             _ => Nil
          };
          if (pts.Length == 0 || outer.Length == 4) return s;
@@ -134,12 +177,33 @@ public readonly struct MinSphere (double radius, Point3 center) {
                (1, _) => MES (pts[..i], [outer[0], pt]),
                (2, false) => From (outer[0], outer[1], pt), // Minimum sphere from 3 points
                (2, _) => MES (pts[..i], [outer[0], outer[1], pt]),
-               (3, false) => From (outer[0], outer[1], outer[2], pt), // Circum-sphere 
                (3, _) => MES (pts[..i], [outer[0], outer[1], outer[2], pt]),
                _ => throw new Exception ("Not expecting this")
             };
          }
          return s;
+      }
+
+      // Searches for the Minimum Enclosing Sphere (MES) from four points.
+      static MinSphere Find (ReadOnlySpan<Point3> pts) {
+         if (pts.Length != 4) throw new ArgumentException ("Expecting four points to compute circumsphere.");
+         // Circumsphere from four points
+         MinSphere best = From (pts[0], pts[1], pts[2], pts[3]);
+         // Find MES from pair of points
+         for (int i = 0; i < pts.Length - 1; i++)
+            for (int j = i + 1; j < pts.Length; j++) 
+               Consider (From (pts[i], pts[j]), pts);
+         // Find MES from triplet of points
+         for (int i = 0; i < pts.Length - 2; i++)
+            for (int j = i + 1; j < pts.Length - 1; j++)
+               for (int k = j + 1; k < pts.Length; k++) 
+                  Consider (From (pts[i], pts[j], pts[k]), pts);
+         return best;
+
+         void Consider (in MinSphere s, ReadOnlySpan<Point3> points) {
+            if ((!best.OK || s.Radius < (best.Radius - Epsilon)) && s.Contains (points)) 
+               best = s;
+         }
       }
    }
 
@@ -157,28 +221,9 @@ public readonly struct MinSphere (double radius, Point3 center) {
       // Transform points from World (X, Y, Z) => Plane (U, V) space.
       Point2 p1 = Point2.Zero, p2 = new (ba.Length, 0), p3 = new (ca.Dot (u), ca.Dot (v));
       // Compute MEC in (U, V) space
-      var C = MEC ([p1, p2, p3]);
+      var C = MinCircle.From ([p1, p2, p3]);
       // Loft center from Plane (U, V) => World (X, Y, Z) space
       return new (C.Radius, a + u * C.Center.X + v * C.Center.Y);
-
-      // Searches for the Minimum Enclosing Circle (MEC) for the given set of points.
-      static MinCircle MEC (ReadOnlySpan<Point2> pts) {
-         if (pts.Length > 3) throw new Exception ("Not expecting this");
-         MinCircle best = MinCircle.Nil;
-         // Find MEC from pair of points
-         for (int i = 0; i < pts.Length - 1; i++)
-            for (int j = i + 1; j < pts.Length; j++)
-               Consider (MinCircle.From (pts[i], pts[j]), pts);
-         
-         Consider (MinCircle.From (pts[0], pts[1], pts[2]), pts);
-         return best;
-
-         void Consider (in MinCircle c, ReadOnlySpan<Point2> pts) {
-            if ((!best.OK || c.Radius < (best.Radius + Epsilon)) && c.Contains (pts)) {
-               best = c;
-            }
-         }
-      }
    }
 
    // Constructs the circumsphere from four non-coplanar points
@@ -201,8 +246,8 @@ public readonly struct MinSphere (double radius, Point3 center) {
          return new (a.X + Dx / D, a.Y + Dy / D, a.Z + Dz / D);
       }
    }
-
+   /// <summary>The uninitialized 'Nil' sphere.</summary>
    public readonly static MinSphere Nil = new (-1, Point3.Nil);
-   readonly double RSqr = (radius * radius) + Epsilon;
+   readonly double RSqr;
 }
 #endregion
--- Demos/WPFDemo/MESDemo.cs
+++ Demos/WPFDemo/MESDemo.cs
@@ -10,32 +10,58 @@ class MinSphereScene : Scene3 {
    public MinSphereScene () {
       BgrdColor = Color4.Gray (96);
       Bound = new Bound3 (0, 0, 0, 800, 800, 800);
-      Root = VN = new RootVN ();
-      Refresh ();
+      Lib.Tracer = TraceVN.Print;
+      Random R = new ();
+      Build ([..GeneratePoints (R, 500, Bound.Width * R.Next (35, 65) / 100)]);
    }
 
-   void Refresh () {
-      Random R = new ();
-      // Generate random points within a cube.
-      int maxpts = 500; double size = Bound.Width / 2;
-      List<Point3> pts = [];
-      Point3 ptRef = new (R.NextDouble () * size, R.NextDouble () * size, R.NextDouble () * size);
-      for (int i = 0; i < maxpts; i++) {
-         var vec = new Vector3 (R.NextDouble (), R.NextDouble (), R.NextDouble ()) * size;
-         pts.Add (ptRef + vec);
-      }
-
-      var s = MinSphere.From (pts.AsSpan ());
+   void Build (ReadOnlySpan<Point3> pts) {
+      var s = MinSphere.From (pts);
       List<Sphere> models = [new (s.Radius, s.Center, Color4.Yellow, true), new (6, s.Center, Color4.Magenta)];
       double rMin = s.Radius - Lib.Epsilon, rMax = s.Radius + Lib.Epsilon;
-      for (int i = 0; i < pts.Count; i++) {
+      int nSurfacePts = 0;
+      for (int i = 0; i < pts.Length; i++) {
          var pt = pts[i]; var d = pt.DistTo (s.Center);
-         var (color, r) = d > rMax ? (Color4.Red, 6) : d < rMin ? (Color4.Blue, 4) : (Color4.Green, 6);
+         var (color, r) = d > rMax ? (Color4.Red, 6) : d < rMin ? (Color4.White, 3) : (Color4.Green, 5);
+         // Skip some of the internal points if there are too many of them.
+         if (pts.Length > 50000 && d < rMin) continue;
          models.Add (new (r, pt, color));
+         if (color.Value == Color4.Green.Value) nSurfacePts++;
       }
-      VN.Set (models);
+      TraceVN.It.Clear ();
+      Lib.Trace ($"Sphere, Radius: {s.Radius.Round (1)}, Center: ({s.Center.X.Round (1)}, {s.Center.Y.Round (1)}, {s.Center.Z.Round (1)})");
+      Lib.Trace ($"Points: {pts.Length}, On Sphere: {nSurfacePts}");
+      Lib.Trace ("Press 'Mininum Sphere' again to regenerate");
+      Root = new GroupVN ([new RootVN (models), TraceVN.It]);
    }
-   RootVN VN;
+
+   static IEnumerable<Point3> GeneratePoints (Random R, int count, double size) {
+      // Generate random points within a cube if size 'size'.
+      Point3 ptRef = new (R.NextDouble () * size, R.NextDouble () * size, R.NextDouble () * size);
+      for (int i = 0; i < count; i++) {
+         var vec = new Vector3 (R.NextDouble (), R.NextDouble (), R.NextDouble ()) * size;
+         yield return ptRef + vec;
+      }
+   }
+
+   class RootVN (List<Sphere> models) : VNode {
+      public override void SetAttributes () => Lux.Color = Color4.Black;
+      public override VNode? GetChild (int n) => n >= Models.Count ? null : new ModelVN (Models[n]);
+
+      public override void Draw () {
+         base.Draw (); Vec3F org = new ();
+         // Draw axis lines.
+         Lux.Lines ([org, new (100, 0, 0), org, new (0, 100, 0), org, new (0, 0, 100)]);
+      }
+
+      readonly List<Sphere> Models = models;
+   }
+
+   class ModelVN (Sphere s) : VNode {
+      public override void SetAttributes () => Lux.Color = S.Color;
+      public override void Draw () => Lux.Mesh (S.Mesh, S.Tranclucent ? EShadeMode.Glass : EShadeMode.Phong);
+      readonly Sphere S = s;
+   }
 
    struct Sphere (double radius, Point3 center, Color4 color, bool translucent = false) {
       public readonly double Radius = radius;
@@ -43,56 +69,41 @@ class MinSphereScene : Scene3 {
       public readonly Color4 Color = color;
       public readonly bool Tranclucent = translucent;
 
-      public Mesh3 Mesh => mMesh ??= BuildMesh (Radius, Center, Radius > 10);
+      public Mesh3 Mesh => mMesh ??= BuildMesh (Radius, Center, Radius <= 10);
       Mesh3? mMesh;
 
-      static Mesh3 BuildMesh (double radius, Point3 center, bool hires) {
+      static Mesh3 BuildMesh (double radius, Point3 center, bool coarse) {
          List<Mesh3.Node> nodes = []; List<int> tries = [];
          // Define the latitude and longitude counts for the sphere.
-         var (lats, longs) = hires ? (36, 72) : (6, 12);
+         var (lats, longs) = coarse ? (6, 12) : (36, 72);
          // Steps in north and east.
          double nstep = PI / lats, estep = Tau / longs, north = 0;
          int rows = longs + 1;
          for (int i = 0; i <= lats; i++, north += nstep) {
-            var theta = i * nstep;
-            var dz = radius * Cos (theta);
-            var rad = radius * Sin (theta);
+            var (dz, r) = (radius * Cos (north), radius * Sin (north));
             double east = 0;
             for (int j = 0; j <= longs; j++, east += estep) {
-               var dx = rad * Cos (east);
-               var dy = rad * Sin (east);
+               var (dx, dy) = (r * Cos (east), r * Sin (east));
                var vec = new Vec3H ((Half)(dx / radius), (Half)(dy / radius), (Half)(dz / radius));
+               // Add mesh node.
                nodes.Add (new (new (center.X + dx, center.Y + dy, center.Z + dz), vec));
+               // Add triangles. Each segment between latitudes consists of two triangles.
+               // ______
+               // |\   |
+               // | \  |
+               // |  \ |
+               // |___\|
                if (i > 0 && j > 0) {
                   int a = (i - 1) * rows + (j - 1), d = a + 1;
                   int b = i * rows + (j - 1), c = b + 1;
                   tries.AddRange ([a, b, c]);
-                  // For the first and the last latitude, only one triangle per segment.
+                  // For the first and the last latitude, at poles, only one triangle per segment.
                   if (i > 1 && i < longs)
                      tries.AddRange ([c, d, a]);
                }
             }
-
          }
-         return new Mesh3 ([.. nodes], [.. tries], []);
+         return new ([.. nodes], [.. tries], []);
       }
-   }
-
-   class RootVN : VNode {
-      public void Set (List<Sphere> models) => mModels = models;
-
-      public override VNode? GetChild (int n) {
-         if (mModels == null || n >= mModels.Count) return null;
-         return new SphereVN (mModels[n]);
-      }
-
-      List<Sphere>? mModels;
-   }
-
-   class SphereVN (Sphere s) : VNode {
-      public override void SetAttributes () => Lux.Color = S.Color;
-      public override void Draw () => Lux.Mesh (S.Mesh, S.Tranclucent ? EShadeMode.Glass : EShadeMode.Phong);
-
-      readonly Sphere S = s;
    }
 }
