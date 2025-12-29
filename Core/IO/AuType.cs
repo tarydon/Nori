ddf76a15eabaed9546fae0cfa415d81a63466969
arvind <arvind@fastmail.in>
2025-12-29 10:37:28 +0530

    Restored AuType.CreateInstance to older behavior

--- Core/IO/AuType.cs
+++ Core/IO/AuType.cs
@@ -155,14 +155,12 @@ class AuType {
    List<AuField>? mUplinks;
 
    // Methods ------------------------------------------------------------------
-   /// <summary>Creates an instance of the object using its parameterless constructor</summary>
    public object CreateInstance () {
-      try {
-         return Activator.CreateInstance (mType)!;
-      } catch (MissingMethodException) {
+      mConstructor ??= mType.GetConstructor (Public | Instance | NonPublic, []) ??
          throw new AuException ($"No parameterless constructor found for {mType.FullName}");
-      }
+      return mConstructor.Invoke ([]);
    }
+   ConstructorInfo? mConstructor;
 
    /// <summary>Get a field of an object, given its name</summary>
    public AuField? GetField (ReadOnlySpan<byte> name) {
