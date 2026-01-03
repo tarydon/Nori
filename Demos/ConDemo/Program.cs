edbe9735246bbbf88ac719355b71640e530697d5
arvind <arvind@fastmail.in>
2026-01-03 19:52:35 +0530

    Removed SurfLab demo

--- Demos/ConDemo/Program.cs
+++ Demos/ConDemo/Program.cs
@@ -3,16 +3,7 @@ namespace ConDemo;
 
 class Program {
    static void Main () {
-      var (bArea, bFile, bId) = (0.0, "", 0);
-      foreach (var file in Directory.GetFiles ("c:/etc/t3")) {
-         // if (!file.Contains ("-003")) continue; 
-         Console.WriteLine (file);
-         var model = new T3XReader (file).Load ();
-         foreach (var ent in model.Ents.OfType<E3RuledSurface> ()) {
-            double area = ent.Area;
-            if (area > bArea) (bArea, bFile, bId) = (area, file, ent.Id);
-         }
-      }
-      Console.WriteLine ($"Biggest NURB {bFile} {bId}");
+      Lib.Init ();
+      Lib.Tracer = Console.WriteLine;
    }
 }
--- Demos/SurfLab/App.xaml.cs
+++
@@ -1,7 +0,0 @@
-using System.Windows;
-
-namespace Surfer;
-/// <summary>Interaction logic for App.xaml</summary>
-public partial class App : Application {
-}
-
