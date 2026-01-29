db678ab5f350953d3bb9709105d3792a5d46da9c
arvind <arvind@fastmail.in>
2026-01-29 10:06:58 +0530

    Started work on Undo

---
+++ Core/Base/Undo.cs
@@ -0,0 +1,43 @@
+﻿namespace Nori;
+
+public abstract class UndoStep {
+   public abstract string Description { get; }
+   public abstract void Step (EUndo dir);
+
+   public void Push () {
+      if (UndoStack.Current is { } stack) stack.Push (this);
+      else Step (EUndo.Redo);
+   }
+}
+
+public enum EUndo { Undo, Redo };
+
+public class UndoStack {
+   public void Push (UndoStep step, bool redoNow = true) {
+      // Since we have an Undo stack, not an undo tree, throw away any undone
+      // actions above the cursor - these can never be redone anymore, we are starting
+      // a new history
+      mSteps.RemoveRange (mCursor + 1, mSteps.Count - mCursor - 1);
+      mSteps.Add (step); mCursor = mSteps.Count - 1;
+      if (redoNow) step.Step (EUndo.Redo);
+   }
+
+   public static UndoStack? Current;
+
+   public UndoStep? NextUndo => mSteps.SafeGet (mCursor);
+   public UndoStep? NextRedo => mSteps.SafeGet (mCursor + 1);
+
+   public void Undo () {
+      if (mCursor > 0) mSteps[mCursor--].Step (EUndo.Undo);
+   }
+
+   public void Redo () {
+      if (mCursor < mSteps.Count - 1) mSteps[++mCursor].Step (EUndo.Redo);
+   }
+
+   // The list of actions (oldest action is at mSteps[0])
+   List<UndoStep> mSteps = [];
+   // Points to the next action to undo (if there is one item that has just
+   // been pushed on, mCursor will be 0)
+   int mCursor = -1;
+}
---
+++ Core/Drawing/DwgActs.cs
@@ -0,0 +1,41 @@
+﻿namespace Nori;
+
+public class ModifyDwgEnts : UndoStep {
+   public ModifyDwgEnts (Dwg2 dwg, string desc, IEnumerable<Ent2> add, IEnumerable<Ent2> rmv) {
+      mDescription = desc;
+      mAdd = [.. add]; mRmv = [.. rmv];
+      mDwg = dwg;
+      QuickStitch ();
+   }
+   readonly Dwg2 mDwg;
+   readonly string mDescription;
+   readonly List<Ent2> mAdd, mRmv;
+
+   public override string Description => mDescription;
+
+   public override void Step (EUndo dir) {
+      var (add, rmv) = dir == EUndo.Redo ? (mAdd, mRmv) : (mRmv, mAdd);
+      foreach (var ent in rmv) mDwg.Ents.Remove (ent);
+      foreach (var ent in add) mDwg.Ents.Add (ent);
+   }
+
+   // Implementation -----------------------------------------------------------
+   // Quick-stitch the incoming entities with existing open poly in the drawing
+   void QuickStitch () {
+      for (int i = mAdd.Count - 1; i >= 0; i--) {
+         // Take each open poly in the new set
+         if (mAdd[i] is not E2Poly { Poly.IsOpen: true } e2p) continue;
+         foreach (var ent in mDwg.Ents) {
+            // Check it against all existing open poly in the same layer
+            if (ent is not E2Poly { Poly.IsOpen: true } e2p0) continue;
+            if (e2p0.Layer != e2p.Layer) continue; 
+            if (e2p0.Poly.TryAppend (e2p.Poly, out var tmp)) {
+               // We are able to append this with the an existing poly e2p0, so
+               // remove that and add in the composite poly instead
+               mRmv.Add (e2p0);
+               mAdd[i] = e2p0.With (tmp);
+            }
+         }
+      }
+   }
+}
