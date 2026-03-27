e2c3c9172c6672f5305e739cf17dac69148d5640
arvind <arvind@fastmail.in>
2026-03-27 10:32:13 +0530

    TEsting with old

--- Core/IO/DXF/AltDXFReader.cs
+++ Core/IO/DXF/AltDXFReader.cs
@@ -46,11 +46,11 @@ public class DXFReader {
       while (Next ()) {
          switch (G) {
             case 0: Type = E; break;
-            case 1: Text = V; break;
+            case 1: Text = ""; break;
             case 3: FontName = V; break;
+            case 8: LayerName = V; break;
             case 6: LTName = V; break;
             case 7: StyleName = V; break;
-            case 8: if (mClosedPoly != null) LayerName = V; break;
             case 9: HeaderVar (E); break;
             case 10: X0 = Vf; break; case 20: Y0 = Vf; break;
             case 11: X1 = Vf; break; case 21: Y1 = Vf; break;
@@ -60,10 +60,10 @@ public class DXFReader {
             case 15: X5 = Vf; break; case 25: Y5 = Vf; break;
             case 16: X6 = Vf; break; 
             case 26: Y6 = Vf; break;
-            case 40: D0 = Vf; break; case 41: D1 = Vf; break; case 42: D2 = Vf; break;
-            case 43: D3 = Vf; break; case 44: D4 = Vf; break; case 45: D5 = Vf; break;
-            case 46: D6 = Vf; break; case 47: D7 = Vf; break; case 48: D8 = Vf; break; 
-            case 50: Ang0 = Vf; break; case 51: Ang1 = Vf; break;
+            case 40: D40 = Vf; break; case 41: D41 = Vf; break; case 42: D42 = Vf; break;
+            case 43: D43 = Vf; break; case 44: D44 = Vf; break; case 45: D45 = Vf; break;
+            case 46: D46 = Vf; break; case 47: D47 = Vf; break; case 48: D48 = Vf; break; 
+            case 50: D50 = Vf; break; case 51: D51 = Vf; break;
             case 60: Invisible = Vn == 1; break;
             case 70: I0 = Vn; break; case 71: I1 = Vn; break; case 72: I2 = Vn; break; 
             case 73: I3 = Vn; break; case 74: I4 = Vn; break; case 77: I7 = Vn; break;
@@ -71,7 +71,7 @@ public class DXFReader {
             case 67: PaperSpace = Vn > 0; break;
             case 141: DimCen = Vf; break;
             case 147: DimGap = Vf; break;
-            case 230: ZFlip = Vf < -0.999; break;
+            case 230: ZDir = Vf < -0.999 ? -1 : 1; break;
             case 1000: if (mType == LINE) mXData.Add (V); break;
 
             case 2:
@@ -96,66 +96,58 @@ public class DXFReader {
       sTypeIgnore = [.. Lib.ReadLines ("nori:DXF/entity-ignore.txt").Select (Enum.Parse<EDXF>)];
    }
 
-   // Adds a Poly to the current layer
    void Add (Poly poly) => Add (new E2Poly (Layer, poly));
 
-   // Add an entity into the drawing
    void Add (Ent2 ent) {
-      ent.Color = GetColor (); 
       if (Invisible) return;
-      if (mBlockEnts != null) { ent.InBlock = true; mBlockEnts.Add (ent); } 
-      else mDwg.Add (ent);
+      ent.Color = GetColor (ColorNo); mDwg.Add (ent);
    }
-
-   // Adds multiple entities into the drawing
-   void Add (IEnumerable<Ent2> ents) => ents.ForEach (Add);
 
    // Builds an entity
    void BuildEnt (EDXF type) {
       if (type > _LASTENT) return;
       switch (type) {
-         case BLOCK: (mBlockEnts, mBlockName, mBlockPt) = ([], Name, Pt0); break;
-         case STYLE: mDwg.Add (new Style2 (Name, FontName, Height, XScale, Angle)); break;
-         case SOLID or TRACE: Add (new E2Solid (Layer, [Pt0, Pt1, Pt2, Pt3])); break;
+         case CIRCLE: Add (Poly.Circle (Pt0, Radius)); break;
+         case STYLE: mDwg.Add (new Style2 (Name, FontName, D40 * Scale, D41 == 0 ? 1 : D41, D50.D2R ())); break;
+         case TRACE or SOLID: Add (new E2Solid (Layer, [Pt0, Pt1, Pt2, Pt3])); break;
 
          case LAYER:
-            bool visible = (Flags & 1) == 0; 
-            var layer = new Layer2 (Name, GetColor (), GetLType ()) { IsVisible = visible };
+            bool visible = (I0 & 1) != 1;
+            var layer = new Layer2 (Name, GetColor (ColorNo), GetLType (LTName)) { IsVisible = visible };
             if (mLayers.TryAdd (Name, layer)) mDwg.Add (layer);
             break;
 
          case LINE:
-            var line = Poly.Line (Pt0, Pt1); 
-            // Try to find bend info among mXData entries. If we do, we create an E2Bendline
-            // object. Otherwise, we create a single-segment line poly
+            var line = Poly.Line (Pt0, Pt1);
+            // Try to find bend info among mXData entries
             var (ba, radius, kfactor) = (double.NaN, 0.0, 0.42);
             foreach (var s in mXData) {
                if (s.StartsWith ("BEND_ANGLE:")) ba = s[11..].ToDouble ().D2R ();
                else if (s.StartsWith ("BEND_RADIUS:")) radius = s[12..].ToDouble () * Scale;
                else if (s.StartsWith ("K_FACTOR:")) kfactor = s[9..].ToDouble ();
             }
-            if (ba.IsNan) Add (line);
-            else {
+            if (!ba.IsNan) {
                Add (new E2Bendline (mDwg, line.Pts, ba, radius, kfactor));
                mXData.Clear ();
-            }
+            } else Add (line);
             break;
 
-         case MTEXT:
-            ETextAlign align = (ETextAlign)(I1 >= 7 ? I1 + 3 : I1);
-            double angle = (X1 == 0 && Y1 == 0) ? Ang0 : Math.Atan2 (Y1, X1);
-            Add (MakeMText (Layer, Style, Text, Pt0, Height, angle, align));
+         case TEXT:
+            int hAlign = I2 > 2 ? 0 : I2.Clamp (0, 2), vAlign = 3 - I3.Clamp (0, 3);
+            ETextAlign align = (ETextAlign)(vAlign * 3 + hAlign + 1);
+            Point2 pos = align == ETextAlign.BaseLeft ? Pt0 : Pt1;
+            Add (new E2Text (Layer, Style, Clean (Text, mSB), pos, Height, Angle, Oblique, XScale, align));
             break;
 
-         //case ARC or CIRCLE: break;
-         //case POINT or LWPOLYLINE or TEXT or DIMENSION or INSERT or SPLINE or POLYLINE: break;
-         //case VERTEX or SEQEND or ATTDEF or ATTRIB or LEADER or TRACE or ELLIPSE or XLINE: break;
+         case BLOCK or SOLID or MTEXT or LINE or ARC : break;
+         case POINT or LWPOLYLINE or DIMENSION or INSERT or SPLINE or POLYLINE: break;
+         case VERTEX or SEQEND or ATTDEF or ATTRIB or LEADER or TRACE or ELLIPSE or XLINE: break;
          default: Fatal ($"Unhandled entity {type}"); break;
       } // TODO: Handle HATCH
    }
 
    // Parses the encoded characters in the text to the corresponding special characters
-   static string Clean (string text, StringBuilder? sb = null) {
+   internal static string Clean (string text, StringBuilder? sb = null) {
       if (!text.Contains ("%%")) return text;
       (sb ??= new ()).Clear ();
       int len = text.Length, i = 0;
@@ -173,23 +165,27 @@ public class DXFReader {
       return sb.ToString ();
    }
 
-   // Converts the current AutoCAD color to a Color4
-   Color4 GetColor () {
-      if (ColorNo == 256) return Color4.Nil;
-      var color = ACADColors[ColorNo.Clamp (0, 255)];
+   /// <summary>Convert an AutoCAD color to a Color4</summary>
+   Color4 GetColor (int index) {
+      if (index == 256) return Color4.Nil;
+      var color = ACADColors[index.Clamp (0, 255)];
       if (WhiteToBlack && color.EQ (Color4.White)) color = Color4.Black;
       if (DarkenColors) color = color.Darkened ();
       return color;
    }
 
-   // Converts the current linetype name to an ELType enumeration
-   ELineType GetLType () => sLineTypes.GetValueOrDefault (LTName, ELineType.Continuous);
-   static Dictionary<string, ELineType> sLineTypes = new (StringComparer.OrdinalIgnoreCase) {
-      ["DOT"] = ELineType.Dot, ["DOTTED"] = ELineType.Dot, ["DASH"] = ELineType.Dash, 
-      ["DASHED"] = ELineType.Dash, ["DASHDOT"] = ELineType.DashDot, ["DIVIDE"] = ELineType.DashDotDot,
-      ["DASHDOTDOT"] = ELineType.DashDotDot, ["DASH2DOT"] = ELineType.DashDotDot, 
-      ["CENTER"] = ELineType.Center, ["BORDER"] = ELineType.Border, ["HIDDEN"] = ELineType.Hidden,
-      ["DASH2"] = ELineType.Dash2, ["PHANTOM"] = ELineType.Phantom
+   /// <summary>Converts a DXF linetype string to the corresponding ELineType enum value </summary>
+   static ELineType GetLType (string lt) => lt.ToUpper () switch {
+      "DOT" or "DOTTED" => ELineType.Dot,
+      "DASH" or "DASHED" => ELineType.Dash,
+      "DASHDOT" => ELineType.DashDot,
+      "DIVIDE" or "DASHDOTDOT" or "DASH2DOT" => ELineType.DashDotDot,
+      "CENTER" => ELineType.Center,
+      "BORDER" => ELineType.Border,
+      "HIDDEN" => ELineType.Hidden,
+      "DASH2" => ELineType.Dash2,
+      "PHANTOM" => ELineType.Phantom,
+      _ => ELineType.Continuous
    };
 
    // Called when we read a header variable
@@ -242,9 +238,9 @@ public class DXFReader {
    }
    // These are the stuff we handle, and the stuff we knowingly ignore
    static readonly HashSet<EDXF> 
-      sHandle = [HEADER, TABLES, BLOCKS, ENTITIES, LTYPE, LAYER, STYLE, DIMSTYLE],
+      sHandle = [HEADER, TABLES, BLOCKS, ENTITIES, LTYPE, LAYER, STYLE, DIMSTYLE, LAYERS],
       sIgnore = [CLASSES, VPORT, UCS, APPID, VIEW, BLOCK_RECORD, OBJECTS, ACDSDATA, THUMBNAILIMAGE, 
-                 DWGMGR, LAYERS];
+                 DWGMGR];
 
    // This is called at the start of each table
    void HandleTable (EDXF t) {
@@ -254,71 +250,10 @@ public class DXFReader {
       } 
    }
 
-   // Extracts text from the encoded MTEXT string and returns the corresponding E2Text entities.
-   IEnumerable<E2Text> MakeMText (Layer2 layer, Style2 style, string text, Point2 pos, double height, double angle, ETextAlign align) {
-      var matches = sRxMText.Matches (text);
-      if (matches.Count > 0) {
-         // First, use the RegEx below to parse and replace various escape sequences in the string
-         mSB.Clear ();
-         int last = 0; var tspan = text.AsSpan ();
-         foreach (Match M in matches) {
-            // We found a match. First, copy all the characters preceding the match
-            // into the target
-            if (M.Index > last) Append2 (tspan[last..M.Index]);
-            if (M.Groups.TryGetValue ("fract", out var fract) && fract.ValueSpan.Length > 0) {
-               // No special fraction rendering is supported. Just concatenate using the division '/' symbol.
-               Append (fract.Value.Replace ("^", "/").Replace ("#", "/"));
-            }
-            if (M.Groups.TryGetValue ("hex4", out var hex4) && hex4.ValueSpan.Length > 0) {
-               // Replace the 4 hex digits with the corresponding unicode character
-               int uni = int.Parse (hex4.Value, NumberStyles.HexNumber);
-               Append (((char)uni).ToString ());
-            }            
-            last = M.Index + M.Length;
-         }
-         if (last < text.Length) Append2 (tspan[last..]);
-         text = mSB.ToString ();
-
-         // Helpers ........................................
-         void Append (string text1) => mSB.Append (text1);
-         void Append2 (ReadOnlySpan<char> text2) => mSB.Append (text2);
-      }
-
-      // Now cleanup the raw-string by removing code-blocks ({...}) and split them into multiple
-      // lines by the line-break (\P). Then, output a text entity for each line
-      double dyLine = 0;
-      string[] lines = text.Replace ("{", "").Replace ("}", "").Split ("\\P");
-      var mat = Matrix2.Rotation (pos, angle);
-      foreach (var line in lines) {
-         var pt = pos;
-         if (!dyLine.IsZero ()) {
-            pt = new (pt.X, pt.Y - dyLine);
-            if (!angle.IsZero ()) pt *= mat;
-         }
-         var ent = new E2Text (layer, style, Clean (line, mSB), pt, height, angle, style.Oblique, style.XScale, align);
-         dyLine += ent.DYLine;
-         yield return ent;
-      }
-   }
-   // The RegEx used to parse various escape-sequences in MText (See Doc\MText-Codes.pdf for a reference).
-   // A MTEXT text entity can specify inline text styles and formatting. The Regex below identifies
-   // the format strings and extracts the raw-text out of them. Multiple patterns are supported, and
-   // each is on a separate line. These patterns are combined using the OR operator. The paragraph
-   // markers (\P) and the style code-blocks ({...}) are left unidentified and are
-   // processed after the text extraction.
-   [SuppressMessage ("Performance", "SYSLIB1045")]
-   static readonly Regex sRxMText = new (
-     @"(\\[Ff][^|;]+((\|([bicp])\d+)+)?;)|" +   // Font name & style (e.g., \fTimes New Roman|b1|i0;)
-     @"(\\[AHWCT](\d*?(\.\d+)?x?));|" +         // Height, Width, Alignment, Color codes like: \H3x; \H12.500; \W0.8x;
-     @"(\\[LlOoKk])|" +                         // Underline, Overstrike, Strikethrough: \L \l \O \K
-     @"\\U\+(?<hex4>[0-9A-Fa-f]{4})|" +         // Match 4 hex digits prefixed with \U+
-     @"(\\S(?<fract>[^;]+[#/\^][^;]+);)",       // Stacking fractions like: \S+0.8^+0.1; \S+0.8#+0.1;
-     RegexOptions.Compiled);
-
    // Reads the next group into mGroup and the value into mValue
    bool Next () {
       for (; ; ) {
-         if (R.AtEndOfFile) return false;
+         // if (R.AtEndOfFile) return false;
          R.Read (out G).SkipToNextLine ();      // Read the group code
          R.ReadLineRange (out mSt, out mLen);   // Read the value
          if (mLen == 3 && D[mSt] == 'E' && D[mSt + 1] == 'O' && D[mSt + 2] == 'F') return false;
@@ -347,10 +282,10 @@ public class DXFReader {
          // then we set the mType to SKIPPEDENT and return. Otherwise, we do a cleanup
          if (value is > _FIRSTENT and < _LASTAUX) {
             // Reset all buffers in preparation for reading this entity
-            I0 = I1 = I2 = I3 = I4 = I7 = 0; D2 = 0; Ang0 = 0; 
-            mX0.ClearFast (); mY0.ClearFast ();
+            I0 = I1 = I2 = I3 = I4 = I7 = 0; D42 = 0; D50 = 0; 
+            mX0.ClearFast (); mY0.ClearFast (); mXData.Clear ();
             mD0.ClearFast (); mD1.ClearFast (); mD2.ClearFast ();
-            PaperSpace = ZFlip = Invisible = false; ColorNo = 256;
+            PaperSpace = Invisible = false; ZDir = 1; ColorNo = 256;
             // TODO: Reset ColorNo if not VERTEX / SEQEND
          } else {
             if (value is > _FIRSTIGNORE and < _LASTIGNORE) mType = SKIPPEDENT;
@@ -365,16 +300,17 @@ public class DXFReader {
    // Computed properties ------------------------------------------------------
    EDXF E => DXFCore.Dict.GetValueOrDefault (D.AsSpan (mSt, mLen));  // Current value, as an EDXF enumeration
 
-   double Angle => Ang0.D2R ();        // Group 50
-   int Flags => I0;                    // Flags value (group 70)   
-   double Height => D0 * Scale;        // Group 10 code
-   Point2 Pt0 => new (X0 * Scale, Y1 * Scale);  // 10,20 group codes
-   Point2 Pt1 => new (X1 * Scale, Y1 * Scale);  // 11,21 group codes
-   Point2 Pt2 => new (X2 * Scale, Y2 * Scale);  // 12,22 group codes
-   Point2 Pt3 => new (X3 * Scale, Y3 * Scale);  // 13,23 group codes
-   Style2 Style => mDwg.GetStyle (StyleName) ?? mDwg.GetStyle ("STANDARD")!;
-   double XScale => D1 == 0 ? 1 : D1;  // Group 41 code (defaults to 1)
-   double YScale => D2 == 0 ? 1 : D2;  // Group 42 code (defaults to 1)
+   int Flags => I0;
+   double Angle => D50.D2R ();
+   double Height => D40 * Scale;
+   double Oblique => D51.D2R ();
+   Point2 Pt0 => new (X0 * Scale, Y0 * Scale);
+   Point2 Pt1 => new (X1 * Scale, Y1 * Scale);
+   Point2 Pt2 => new (X2 * Scale, Y2 * Scale);
+   Point2 Pt3 => new (X3 * Scale, Y3 * Scale);
+   double Radius => D40 * Scale;
+   double XScale => D41 == 0 ? 1 : D41;
+   double YScale => D42 == 0 ? 1 : D42;
 
    ReadOnlySpan<byte> SP => D.AsSpan (mSt, mLen);
    string V => mEncoding.GetString (D.AsSpan (mSt, mLen));           // Current value, as a string
@@ -383,36 +319,33 @@ public class DXFReader {
 
    // Storage properties -------------------------------------------------------
    int ColorNo;
-   double Ang0, Ang1;
-   int I0, I1, I2, I3, I4, I7;
-   double D3, D4, D5, D6, D7, D8;
-   double X1, Y1, X2, X4, X5, X6, Y2, X3, Y3, Y4, Y5, Y6;
+   double D50, D51;
    bool PaperSpace, Invisible;
+   int I0, I1, I2, I3, I4, I7;
+   double D43, D44, D45, D46, D47, D48;
+   double X1, Y1, X2, X4, X5, X6, Y2, X3, Y3, Y4, Y5, Y6;
    double DimCen, DimGap;
-   double D0 { get => field; set => mD0.Add (field = value); }
-   double D1 { get => field; set => mD1.Add (field = value); }
+   double D40 { get => field; set => mD0.Add (field = value); }
+   double D41 { get => field; set => mD1.Add (field = value); }
    double X0 { get => field; set => mX0.Add (field = value); } 
    double Y0 { get => field; set => mY0.Add (field = value); }
    List<double> mX0 = [], mY0 = [], mD0 = [], mD1 = [], mD2 = [];
    string Name = "", LTName = "", StyleName = "", FontName = "", Text = "";
-   bool ZFlip;
+   double ZDir = 1;
 
-   // Whenever we read in a new layername, we set this 
-   string LayerName { set { if (field != value) mLayer = mLayers[field = value]; } } = "";
-   Layer2 Layer => mLayer ?? mDwg.CurrentLayer;
-   Layer2? mLayer;
-
-   // Group 42 is also used for the current bulge factor (in VERTEX entities)
-   // This can be elided for some VERTEX objects (and only X0 and Y0 may be supplied).
-   // So when adding a new bulge, we make sure the mD2 list has the same number of 
-   // elements as the mX0 list (filling the missing elements with bulge=0 as needed)
-   double D2 {
+   double D42 {
       get => field;
       set {
          while (mD2.Count < mX0.Count - 1) mD2.Add (0);
          mD2.Add (field = value);
       }
    }
+
+   Style2 Style => mDwg.GetStyle (StyleName) ?? mDwg.GetStyle ("STANDARD")!;
+   string LayerName { set { if (field != value) { mLayer = mLayers.GetValueOrDefault (field = value); } } }
+   Dictionary<string, Layer2> mLayers = new (StringComparer.OrdinalIgnoreCase);
+   Layer2 Layer => mLayer ?? mDwg.CurrentLayer;
+   Layer2? mLayer;
 
    // Debug code ---------------------------------------------------------------
    [DoesNotReturn] void Fatal (string s) => throw new ($"At line {R.LineNo - 2}: {s}");
@@ -433,21 +366,17 @@ public class DXFReader {
    HashSet<string>? sHeaderIgnore;
 
    void Warn (string s) { if ((sWarnings ??= []).Add (s)) Lib.Trace (s); }
-   HashSet<string>? sWarnings;
+   HashSet<string>? sWarnings; 
 
    // Private data -------------------------------------------------------------
-   int G;                        // Group code
-   int mSt, mLen;                // Start and length of the current line
-   readonly Dwg2 mDwg = new ();   // The drawing we're building
+   readonly Dwg2 mDwg = new ();  // The drawing we're building
    readonly byte[] D;            // The raw data of the line
    readonly UTFReader R;         // The UTFReader used to read the file
+   int G;                        // Group code
+   int mLineNo;                  // The line number
+   int mSt, mLen;                // Start and length of the current line
    static SearchValues<byte> sCRLF = SearchValues.Create (13, 10);
    Encoding mEncoding = Encoding.UTF8;       // The encoding we're using
-   Dictionary<string, Layer2> mLayers = [];  // Dictionary mapping names to layers
-   string mBlockName = "*";      // Name of the block we're building
-   List<Ent2>? mBlockEnts;       // List of entities in that block
-   Point2 mBlockPt;              // Insert point of that block
-   bool? mClosedPoly;            // NULL=not-in-poly, true=closed-poly, false=open-poly
-   readonly List<string> mXData = [];     // Group 1000 codes (like BEND_ANGLE:90)
-   readonly StringBuilder mSB = new ();   // Stringbuilder used in various operations
+   List<string> mXData = [];     // Strings loaded from 1000 group (used for bend-data)
+   readonly StringBuilder mSB = new ();      
 }
--- Demos/ConShell/Program.cs
+++ Demos/ConShell/Program.cs
@@ -2,6 +2,7 @@
 // ╔═╦╦═╦╦╬╣ Program.cs
 // ║║║║╬║╔╣║ Shell for Nori console scratch applications
 // ╚╩═╩═╩╝╚╝ ───────────────────────────────────────────────────────────────────────────────────────
+using System.Diagnostics;
 using Nori;
 namespace ConShell;
 
@@ -10,8 +11,36 @@ class Program {
       Lib.Init ();
       Lib.Tracer = Console.WriteLine;
 
-      Nori.Alt.DXFReader dr = new ("N:/TData/IO/DXF/AllEnts.dxf");
-      var dwg = dr.Load ();
+      Compare ();
+
+      //Nori.Alt.DXFReader dr = new ("N:/TData/IO/DXF/AllEnts.dxf");
+      //var dwg = dr.Load ();
+   }
+
+   static void Compare () {
+      var files = Directory.GetFiles ("W:\\DXF", "*.dxf")
+                           .Select (a => new FileInfo (a))
+                           .OrderBy (a => a.Length)
+                           .Select (a => a.FullName)
+                           .ToArray ();
+      for (int i = 0; i < files.Length; i++) {
+         if ((i % 1000) == 0) Console.Write (i + " ");
+         var file = files[i];
+         File.Copy (file, "c:\\etc\\compare\\test.dxf", true);
+         Console.Write (file + " ");
+         var dwg1 = DXFReader.Load (file);
+         var dwg2 = new Nori.Alt.DXFReader (file).Load ();
+         CurlWriter.Save (dwg1, "c:/etc/compare/old.curl", Path.GetFileNameWithoutExtension (file));
+         CurlWriter.Save (dwg2, "c:/etc/compare/new.curl", Path.GetFileNameWithoutExtension (file));
+         if (File.ReadAllText ("c:/etc/compare/old.curl") == File.ReadAllText ("c:/etc/compare/new.curl")) {
+            Console.Write ("SAME");
+            File.Move (file, "W:\\DXF\\SAME\\" + Path.GetFileName (file));
+         } else {
+            Process.Start ("winmergeu.exe", "c:\\etc\\compare\\old.curl c:\\etc\\compare\\new.curl");
+            break;
+         }
+         Console.WriteLine ();
+      }
    }
 
    static void LoadAllDXFs () {
