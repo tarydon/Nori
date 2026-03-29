15d3b73fc83f3be75463bf274ee239986a993c63
arvind <arvind@fastmail.in>
2026-03-29 15:18:07 +0530

    Step 2: Working on building faces

--- Core/Folder.cs
+++ Core/Folder.cs
@@ -11,6 +11,7 @@ public class Folder {
    public bool Process () {
       if (!GatherContours ()) return false;
       SnapBendlines ();
+      for (int i = 0; i < mNode.Length; i++) MakeFace (i);
       return false;
    }
 
@@ -21,7 +22,18 @@ public class Folder {
          var bl1 = bend.BLine;
          var bl2 = new E2Bendline (dwg, bend.Pts, bl1.Angle, bl1.Radius, bl1.KFactor, bl1.Thickness);
          dwg.Add (bl2);
-         foreach (var pt in bend.Pts) dwg.Add (pt);
+         for (int k = 0; k < bend.Pts.Length; k++) {
+            ref Node node = ref mNode[bend.NBase + k];
+            var poly = mPolys[node.NPoly].Poly;
+            var seg = poly[node.NSeg];
+            var pt = seg.GetPointAt (node.Lie);
+            dwg.Add (pt); dwg.Add (Poly.Circle (pt, 2));
+         }
+      }
+
+      foreach (var n in mSorted) {
+         var node = mNode[n];
+         Console.WriteLine ($"{node.NPoly,4} {node.NSeg,4}  {node.Lie.Round (2)}");
       }
       DXFWriter.Save (dwg, file);
    }
@@ -32,12 +44,10 @@ public class Folder {
       // Gather all the closed poly, with the outer one being at 0. The outer poly
       // is CCW, while the holes are CW
       if (!mDwg.MarkInOut ()) return false;
-      
+
       // Gather all the closed Poly, and move the outer contour to index 0
-      var tmp = mDwg.Ents.OfType<E2Poly> ()
-                    .Where (a => a.Poly.IsClosed && a.Poly.GetWinding () != Poly.EWinding.Indeterminate)
-                    .ToList ();
-      for (int i = 1; i < tmp.Count; i++) 
+      List<E2Poly> tmp = [.. mDwg.Ents.OfType<E2Poly> ().Where (Accept)];
+      for (int i = 1; i < tmp.Count; i++)
          if (tmp[i].IsOuter) { (tmp[i], tmp[0]) = (tmp[0], tmp[i]); break; }
       mPolys = new CPoly[tmp.Count];
       for (int i = 0; i < tmp.Count; i++) {
@@ -45,18 +55,43 @@ public class Folder {
          var poly = e2p.Poly;
          var wind = poly.GetWinding ();
          if (e2p.IsOuter ^ wind == Poly.EWinding.CW) poly = poly.Reversed ();
-         mPolys[i] = new (i, poly, e2p.Bound);
+         mPolys[i] = new (poly, e2p.Bound);
       }
 
       // Gather all the bendlines
-      var tmp2 = mDwg.Ents.OfType<E2Bendline> ().ToList ();
-      mBends = new Bend[tmp2.Count];
-      for (int i = 0; i < tmp2.Count; i++) mBends[i] = new Bend (tmp2[i]);
+      List<E2Bendline> tmp2 = [.. mDwg.Ents.OfType<E2Bendline> ()];
+      mBends = new Bend[tmp2.Count]; int nBase = 0; 
+      for (int i = 0; i < tmp2.Count; i++) {
+         mBends[i] = new Bend (tmp2[i], nBase);
+         nBase += mBends[i].Pts.Length;
+      }
+      mNode = new Node[nBase];
       return true;
+
+      // Helpers ...........................................
+      static bool Accept (E2Poly e2p)
+         => e2p.Poly.IsClosed && e2p.Poly.GetWinding () != Poly.EWinding.Indeterminate;
+   }
+
+   // Create faces from the nodes
+   void MakeFace (int n) {
+      ref Node node = ref mNode[n]; if (node.Used) return;
+
+      // We're going to start building a face
+      PolyBuilder pb = new ();
+      bool bend = node.Used = true;
+      for (; ; ) {
+         if (bend) {
+            // We travel along the bendline
+         } else {
+            // We travel along the contour
+         }
+      }
    }
 
    // Snaps bend-lines to begin/end exactly on contours (by trimming / extending) them
    void SnapBendlines () {
+      int nSeg = 0, nPoly = 0; double dist;
       Span<Point2> buffer = stackalloc Point2[2];
       for (int i = 0; i < mBends.Length; i++) {
          ref Bend bend = ref mBends[i];
@@ -72,50 +107,84 @@ public class Folder {
             // First step: if the endpoint is already snapped to an existing contour, don't
             // do anything (just record the Poly/Seg there and continue)
             Point2 pt = pts[k];
+            ref Node node = ref mNode[bend.NBase + k];
+            (node.NBend, node.NPt) = (i, k);
             for (int j = 0; j < mPolys.Length; j++) {
                ref CPoly con = ref mPolys[j]; if (!con.Intersects) continue;
-               var (dist, nSeg1) = con.Poly.GetDistance (pt);
-               if (dist.IsZero ()) { bend.Poly[k] = j; bend.Seg[k] = nSeg1; break; }
+               (dist, nSeg) = con.Poly.GetDistance (pt);
+               if (dist.IsZero ()) {
+                  (node.NPoly, node.NSeg, node.Lie) = (j, nSeg, con.Poly[nSeg].GetLie (pt));
+                  break;
+               }
             }
-            if (bend.Poly[k] != -1) continue;
+            if (node.NPoly == -1) continue; 
 
             // Otherwise, snap the bendline to the closest intersection of any of the poly
-            double distMin = 1e99; int nPoly = -1, nSeg2 = -1; Point2 pBest = pt;
+            double distMin = 1e99; Point2 pBest = pt;
             for (int j = 0; j < mPolys.Length; j++) {
                ref CPoly con = ref mPolys[j]; if (!con.Intersects) continue;
                foreach (var seg in con.Poly.Segs) {
                   var ints = seg.Intersect (a, b, buffer, true);
                   foreach (var pint in ints) {
-                     double dist = pint.DistToSq (pt);
-                     if (dist < distMin) (distMin, nPoly, nSeg2, pBest) = (dist, j, seg.N, pint);
+                     dist = pint.DistToSq (pt);
+                     if (dist < distMin) (distMin, nPoly, nSeg, pBest) = (dist, j, seg.N, pint);
                   }
                }
             }
-            bend.Pts[k] = pBest; bend.Poly[k] = nPoly; bend.Seg[k] = nSeg2;
+            // Copy the position and location into index k
+            (node.NPoly, node.NSeg) = (nPoly, nSeg);
+            node.Lie = mPolys[nPoly].Poly[nSeg].GetLie (bend.Pts[k] = pBest);
          }
+      }
+      mSorted = [.. Enumerable.Range (0, mNode.Length)];
+      mSorted.Sort (CompareNode);
+      for (int i = 0; i < mSorted.Length; i++) {
+         int j = mSorted[(i + 1) % mSorted.Length];
+         ref Node node = ref mNode[mSorted[i]];
+         node.Next = j; 
+      }
+
+      // Helpers ...........................................
+      int CompareNode (int a, int b) {
+         ref Node na = ref mNode[a], nb = ref mNode[b];
+         int n = na.NPoly.CompareTo (nb.NPoly); if (n != 0) return n;
+         n = na.NSeg.CompareTo (nb.NSeg); if (n != 0) return n;
+         return na.Lie.CompareTo (nb.Lie);
       }
    }
 
    // Nested types -------------------------------------------------------------
    // Represents an outer / inner contour of the poly.
    // The outer contour is CCW, while the rest are CW
-   struct CPoly (int n, Poly poly, Bound2 bound) {
-      public readonly int N = n;
+   struct CPoly (Poly poly, Bound2 bound) {
       public readonly Poly Poly = poly;
       public readonly Bound2 Bound = bound;
-      public bool Intersects;  
+      public bool Intersects;
    }
+
+   // Node is a junction between a poly and bendline.
+   // This is a location where a bendline touches a poly (at its endpoint). 
+   struct Node {
+      public int NBend;    // Index of the bendline (within the mBends array)
+      public int NPt;      // Index within the Pts array of that bendline
+      public int NPoly;    // Index of poly (with the mPolys array)
+      public int NSeg;     // Segment number within that poly
+      public double Lie;   // Lie within that segment
+      public int Next;     // Next node within this Poly
+      public bool Used;    // Have we used this already
+   }
+   Node[] mNode = [];
+   int[] mSorted = [];
 
    // Represents a bend-line 
    readonly struct Bend {
-      public Bend (E2Bendline bend) {
+      public Bend (E2Bendline bend, int nBase) {
          Pts = [..(BLine = bend).Pts];
-         Poly = new int[Pts.Length]; Seg = new int[Pts.Length];
-         for (int i = 0; i < Poly.Length; i++) Poly[i] = -1;
+         NBase = nBase;
       }
       public readonly E2Bendline BLine;
-      public readonly Point2[] Pts;
-      public readonly int[] Poly, Seg;
+      public readonly Point2[] Pts;    // REMOVETHIS
+      public readonly int NBase;
    }
 
    // Private data -------------------------------------------------------------
