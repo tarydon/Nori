b00ae2e36686df7be44b6d93688a3caea6f5daf4
naveen <naveen.kujur@trumpf.com>
2026-04-02 11:33:49 +0530

    Prefer  over

--- Core/IO/DXF/DXFReader.cs
+++ Core/IO/DXF/DXFReader.cs
@@ -293,7 +293,8 @@ public partial class DXFReader {
       switch (key) {
          case _ACADVER: mACADVer = V; break;
          case _CLAYER: mCurrentLayer = V; break;
-         case _MEASUREMENT: Scale = Vn == 0 ? 25.4 : 1; break;
+         case _MEASUREMENT: if (!mGotInsUnits) Scale = Vn == 0 ? 25.4 : 1; break;
+         case _INSUNITS: if (Vn is 1 or 4) { Scale = Vn == 1 ? 25.4 : 1; mGotInsUnits = true; } break;
 
          case _DWGCODEPAGE:
             mCodePage = V;
@@ -318,6 +319,7 @@ public partial class DXFReader {
    }
    static bool mEncodingsRegistered;
    string mACADVer = "", mCodePage = "", mCurrentLayer = "";
+   bool mGotInsUnits = false; // Accords higher priority to $INSUNITS over $MEASUREMENT
    double Scale = 1; 
 
    // This is called at the start of each section. 
