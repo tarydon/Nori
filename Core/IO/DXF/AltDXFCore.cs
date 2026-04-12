5765d801aa2690887df3cb0cd6ccde80aba8e08c
arvind <arvind@fastmail.in>
2026-04-12 09:25:21 +0530

    02. Continuing

---
+++ Core/IO/DXF/AltDXFCore.cs
@@ -0,0 +1,62 @@
+﻿namespace Nori.Alt;
+using static EDXF;
+
+// enum EDXF -------------------------------------------------------------------
+enum EDXF {
+   NIL, SKIPPEDENT,
+
+   // These are the entities we are going to try and read (this also includes things like
+   // LAYER, STYLE etc that don't reside in the ENTITIES section, but in other sections such
+   // as the TABLES section)
+   _FIRSTENT,
+   LAYER, STYLE, BLOCK, LINE, SOLID, MTEXT, POINT, ARC, CIRCLE, LWPOLYLINE, TEXT, DIMENSION,
+   INSERT, SPLINE, POLYLINE, VERTEX, SEQEND, ATTRIB, LEADER, TRACE, ELLIPSE, XLINE, ENDBLK,
+   _LASTENT,
+
+   // These are the other objects (not entities) that we are going to not skip over
+   _FIRSTAUX,
+   SECTION, ENDSEC, TABLE, ENDTAB, DIMSTYLE,
+   _LASTAUX,
+
+   // These are all the entities / tables / sections that we are going to ignore   
+   _FIRSTIGNORE,
+   CLASS, LTYPE, VIEWPORT, IMAGE, HATCH, TOLERANCE, ACAD_CIRCLE, ACAD_LINE, _3DFACE, _3DSOLID,
+   ACAD_TABLE, OLE2FRAME, OLEFRAME, ACAD_PROXY_ENTITY, REGION, TCPOINTENTITY, ASSURFACE, WIPEOUT,
+   Point2, BODY, SURFACE, LIGHT, ATTDEF,
+   _LASTIGNORE,
+
+   // Header values we are going to read
+   _ACADVER, _DWGCODEPAGE, _MEASUREMENT, _CLAYER, _INSUNITS,
+
+   // Miscellaneous
+   APPID, BLOCK_RECORD, HELIX, MESH, SUN, UCS, UNDERLAY, VIEW, VPORT, OBJECTS,
+   ACDSDATA, THUMBNAILIMAGE, DWGMGR, HEADER, DICTIONARY, XRECORD, DIMASSOC, LAYOUT, MATERIAL,
+   MLEADERSTYLE, MLINESTYLE, SCALE, TABLES, BLOCKS, ENTITIES, CLASSES, LAYERS, BYBLOCK,
+   ACDBDICTIONARYWDFLT, ACDBDETAILVIEWSTYLE, ACDBPLACEHOLDER, ACDBSECTIONVIEWSTYLE, TABLESTYLE,
+   VISUALSTYLE, DICTIONARYVAR, CELLSTYLEMAP, ACDSSCHEMA, ACDSRECORD, BYLAYER,
+
+   EOF
+}
+
+// class DXFCore ---------------------------------------------------------------
+public class DXFCore {
+   internal static SymTable<EDXF> Dict {
+      get {
+         if (sDict == null) {
+            sDict = new ();
+            for (var ed = _FIRSTENT; ed <= EOF; ed++) {
+               var s = ed.ToString ();
+               if (s.StartsWith ("_3")) s = s[1..];
+               else if (s[0] == '_') s = s.Replace ('_', '$');
+               sDict.Add (s, ed);
+            }
+         }
+         return sDict;
+      }
+   }
+   static SymTable<EDXF>? sDict;
+
+   internal static Encoding GetEncoding (string codepage) {
+
+   }
+}
--- Core/IO/DXF/AltDXFReader.cs
+++ Core/IO/DXF/AltDXFReader.cs
@@ -1,4 +1,5 @@
 ﻿namespace Nori.Alt;
+using static EDXF;
 
 public class DXFReader {
    // Constructors -------------------------------------------------------------
@@ -12,17 +13,44 @@ public class DXFReader {
    /// Parse the file, build the DXF and return it
    /// </summary>
    public Dwg2 Load () {
-      while (Next ()) {
-         if (G == 0) {
-            Console.Write ($"{S (G)}");
-            Console.Write ("  ");
+      EDXF type;
+      while ((type = NextObject ()) != EOF) {
+         switch (type) {
+            case SECTION: LoadSection (); break;
+            default: Console.Write ($"{type} "); break;
          }
       }
-      Console.WriteLine (); 
       return mDwg;
    }
 
    // Implementation -----------------------------------------------------------
+   void LoadSection () {
+      // We need special processing only for the HEADER section, so we do that here. 
+      Next (); Check (G == 2);
+      if (E (2) != HEADER) return;
+      for (; ; ) {
+         if (!Next () || G == 0) break;
+         if (G == 9) {
+            var name = E (9);
+            Next ();
+            switch (name) {
+               case _ACADVER: mACADVer = S (G).ToUpper (); break;
+               case _CLAYER: mCurrentLayer = S (G); break;
+               case _MEASUREMENT: if (!mUnitsSet) Scale = N (G) == 0 ? 25.4 : 1; break;
+               case _INSUNITS:
+                  int n = N (G);
+                  if (n is 1 or 4) { Scale = n == 1 ? 25.4 : 1; mUnitsSet = true; }
+                  break;
+
+               case _DWGCODEPAGE:
+                  if (string.CompareOrdinal (mACADVer, "AC1021") < 0) 
+                     mEncoding = DXFCore.GetEncoding (S (G));
+                  break;
+            }
+         }
+      }
+   }
+
    // Reads the next group code into mG and the corresponding value span into 
    // mSt[G],mLen[G]. If the variable mSkipForward is set, then this keeps reading key-value
    // pairs until a group 0 has been read.
@@ -45,6 +73,15 @@ public class DXFReader {
    // group 0 is read (and that is stored in mG along with its value)
    bool mSkipForward;
 
+   // Reads forward until the next group 0 code, and returns the EDXF tag for the object
+   // found there
+   EDXF NextObject () {
+      mSkipForward = true;
+      if (!Next ()) return EOF;
+      Check (G == 0);      
+      return E (0);
+   }
+
    // Routines to fetch group values -------------------------------------------
    // The mSt[] and mLen[] arrays store the last read values of each of the group codes from 
    // 0..255. Suppose we ask for the value of group 41 code (which is used for Vertex.Bulge),
@@ -55,6 +92,9 @@ public class DXFReader {
    // set to the start of each entity position when we start reading it), and discard any group
    // values whose start points are before this
 
+   // Group value as an EDXF enum
+   EDXF E (int g) => DXFCore.Dict.GetValueOrDefault (SB (g));
+
    // Get the group value N as a span of bytes (if this group value was not present for this entity,
    // this returns an empty (zero-length) span)
    ReadOnlySpan<byte> SB (int g) {
@@ -62,15 +102,29 @@ public class DXFReader {
       return mD.AsSpan (start, mLen[g]);
    }
 
+   // Group value G as an integer
+   int N (int g) => SB (g).ToInt ();
+
    // Group value G as a string
    string S (int g) => mEncoding.GetString (SB (g));
+
+   // Error handling routines --------------------------------------------------
+   void Check (bool condition) {
+      if (!condition) throw new InvalidOperationException ();
+   }
+
+   // DXF state ----------------------------------------------------------------
+   string mACADVer = "AC1021";
+   string mCurrentLayer = "";
+   bool mUnitsSet = false;
+   double Scale = 1; 
 
    // Private data -------------------------------------------------------------
    readonly byte[] mD;           // Raw data of the file
    readonly UTFReader mR;        // UTFReader used to read the file
    readonly Dwg2 mDwg = new ();  // The drawing we're constructing
 
-   int G;                       // Group code 
+   int G;                        // Group code 
    int mBase;                    // The current 'entity' (Group 0 set) data starts here
    int[] mSt = new int[256];     // For each group code, the start of the value
    int[] mLen = new int[256];    // .. and the length of that value (both in D)
