f883a483d8700eea4d6e0e4e51f1edb7e43f5ee6
arvind <arvind@fastmail.in>
2026-07-03 09:37:12 +0530

    Minor 4

--- Test/Misc/TMisc.cs
+++ Test/Misc/TMisc.cs
@@ -198,9 +198,9 @@ class TMisc {
       Lib.GetArcSteps (10, Lib.PI, 0.01, 1.05).Is (36);
       int a = 3, b = 2; Lib.Sort (ref a, ref b);
       a.Is (2); b.Is (3);
-      Lib.ReadText ("nori:GL/Shader/arrowhead.frag").Length.Is (240);
-      Lib.ReadBytes ("nori:GL/Shader/arrowhead.frag").Length.Is (251);
-      Lib.ReadLines ("nori:GL/Shader/arrowhead.frag").Length.Is (12);
+      Lib.ReadText ("nori:GL/Shader/ArrowHead.frag").Length.Is (240);
+      Lib.ReadBytes ("nori:GL/Shader/ArrowHead.frag").Length.Is (251);
+      Lib.ReadLines ("nori:GL/Shader/ArrowHead.frag").Length.Is (12);
       double aa = 1.5; Lib.Set (ref aa, 1.5).IsFalse ();
       Lib.Set (ref aa, 2.5).IsTrue (); aa.Is (2.5);
 
@@ -465,7 +465,7 @@ class TMisc {
 
    [Test (148, "Plane intersection with mesh")]
    public void Test17 () {
-      var mesh = new STLReader (NT.File ("Misc/cali-bee.STL")).BuildMesh ();
+      var mesh = new STLReader (NT.File ("Misc/cali-bee.stl")).BuildMesh ();
       var plane = new PlaneDef (Point3.Zero, Vector3.YAxis);
       List<Polyline3> curves = [];
       new MeshSlicer ([mesh]).Compute (plane, curves);
